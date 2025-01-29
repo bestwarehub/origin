219f7f2f83f927f8161004f236a0a68cd1baacbf
BestWareHub <bestwarehub@gmail.com>
2025-01-30 00:45:28 +0600

    route

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,21 +1,6 @@
 import { dbConnect } from "@/lib/dbConnect";
-import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const _id = searchParams.get("_id");
-
+export async function GET() {
   await dbConnect();
-  try {
-    if (_id) {
-      const data = await Tag.findById(_id).lean();
-      return NextResponse.json({ data }, { status: 200 });
-    }
-    const data = await Tag.find({ status: "publish" })
-      .sort({ createdAt: -1 })
-      .lean();
-    return NextResponse.json({ data }, { status: 200 });
-  } catch (err) {
-    return NextResponse.json({ err }, { status: 500 });
-  }
+      return Response.json({ message: "message" }, { status: 200 });
 }
